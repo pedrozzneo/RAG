88442643a566052848600fdadc4fce6cece1b5e2
Pedro Neo <pedrozzneo@gmail.com>
2026-03-24 22:54:05 -0300

    chore: remove comments

--- utils.py
+++ utils.py
@@ -1,10 +1,6 @@
 import re
 
 def extract_bib_field(entry_text: str, field_name: str) -> str:
-    """
-    Extract a BibTeX field value (e.g. title, abstract, keywords) from a single entry.
-    Supports multiline fields wrapped in { } or " ".
-    """
     pattern = rf"{field_name}\s*=\s*[\{{\"](?P<val>.*?)[\}}\"][\s,]*$"
     m = re.search(pattern, entry_text, flags=re.IGNORECASE | re.DOTALL | re.MULTILINE)
     if not m:
