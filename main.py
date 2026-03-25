fc78d3c3097447170bbee2ad92d5574a2bf44d1b
Pedro Neo <pedrozzneo@gmail.com>
2026-03-24 22:54:52 -0300

    refactor: simplify main

--- main.py
+++ main.py
@@ -5,5 +5,4 @@ def main():
     bib_path = os.path.join("db_source", "one.bib")
     first_criteria_path = apply_first_criteria_with_llm(bib_path)
     print(f"First criteria application file: {first_criteria_path}")
-if __name__ == "__main__":
-    main()
+main()
