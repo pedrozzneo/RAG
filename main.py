a40e6364152fbe446c15f09aae46952c61010888
Pedro Neo <pedrozzneo@gmail.com>
2026-03-26 10:26:39 -0300

    feat: save db source in excel

--- main.py
+++ main.py
@@ -1,7 +1,9 @@
 import os
 from LLMs.GPT import apply_first_criteria_with_llm
+from excel import save_db_source_in_excel
 
 def main():
     bib_path = os.path.join("db_source", "one.bib")
+    save_db_source_in_excel(bib_path)
     apply_first_criteria_with_llm(bib_path)    
 main()
