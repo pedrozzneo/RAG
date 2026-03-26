2e347a292976cac705ab7cb6d6684218959f5a79
Pedro Neo <pedrozzneo@gmail.com>
2026-03-26 09:57:43 -0300

    refactor: simplify code

--- main.py
+++ main.py
@@ -3,6 +3,5 @@ from LLMs.GPT import apply_first_criteria_with_llm
 
 def main():
     bib_path = os.path.join("db_source", "one.bib")
-    first_criteria_path = apply_first_criteria_with_llm(bib_path)
-    print(f"First criteria application file: {first_criteria_path}")
+    apply_first_criteria_with_llm(bib_path)    
 main()
