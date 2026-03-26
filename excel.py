fc3a4f4b0c9043a3779330e9f2d0e09866daf3b3
Pedro Neo <pedrozzneo@gmail.com>
2026-03-26 10:45:36 -0300

    feat: take the values from the real db, no longer mocked

--- excel.py
+++ excel.py
@@ -1,4 +1,5 @@
 import pandas as pd
+from utils import getStudies, extract_bib_field
 
 def save_llm_results_in_excel(ai, index, criterias, status):
     dataFrame = pd.read_excel("results/excelResults.xlsx")
@@ -10,14 +11,28 @@ def save_llm_results_in_excel(ai, index, criterias, status):
     
 # save_llm_results_in_excel("GPT", 0, "IC4", "INCLUDED")
 
-def save_db_source_in_excel(index, title, dbCriterias, dbStatus):
-    dataFrame = pd.read_excel("results/excelResults.xlsx")
+def save_db_source_in_excel(dbPath):
+    with open(dbPath) as f: 
+        text = f.read()
 
-    dataFrame.loc[index, "title"] = title
-    dataFrame.loc[index, "db criterias"] = dbCriterias
-    dataFrame.loc[index, "db status"] = dbStatus
+    studies = getStudies(text)
 
-    dataFrame.to_excel("results/excelResults.xlsx", index=False)
+    for index, study in enumerate(studies):
+        title = extract_bib_field(study, "title")
+        dbCriterias = extract_bib_field(study, "criteria")
+        dbStatus = extract_bib_field(study, "status")
 
-for i in range(5):
-    save_db_source_in_excel(i, "test", "IC2", "INCLUDED")
+        dataFrame = pd.read_excel("results/excelResults.xlsx")
+
+        # Solve dtypes issues when the columns are empty
+        dataFrame["title"] = dataFrame["title"].astype("object")
+        dataFrame["db criterias"] = dataFrame["db criterias"].astype("object")
+        dataFrame["db status"] = dataFrame["db status"].astype("object")
+
+        dataFrame.loc[index, "title"] = title
+        dataFrame.loc[index, "db criterias"] = dbCriterias
+        dataFrame.loc[index, "db status"] = dbStatus
+
+        dataFrame.to_excel("results/excelResults.xlsx", index=False)
+        
+save_db_source_in_excel("db_source/one.bib")
