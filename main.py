1570daa775530675027200833bd394abfa2f6995
Pedro Neo <pedrozzneo@gmail.com>
2026-03-26 10:46:34 -0300

    feat: main calls the save db function

--- main.py
+++ main.py
@@ -5,5 +5,5 @@ from excel import save_db_source_in_excel
 def main():
     bib_path = os.path.join("db_source", "one.bib")
     save_db_source_in_excel(bib_path)
-    apply_first_criteria_with_llm(bib_path)    
+    # apply_first_criteria_with_llm(bib_path)    
 main()
